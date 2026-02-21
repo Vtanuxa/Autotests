9872ab1cb2d0d8508665e237354117aa0dd57698
Tatiana <vtanuxa@gmail.com>
2026-02-21 18:14:14 +0300

    update

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,4 +1,26 @@
 package tests;
 
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class LoginTest {
+
+    @Test
+    public  void loginWithValidCred() {
+        open("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        SelenideElement usernameField = $(By.name ("username"));
+        SelenideElement passwordField = $(By.name ("password"));
+        SelenideElement buttonLogin = $(By.xpath("//button[@type='submit']"));
+
+        usernameField.setValue("Admin");
+        passwordField.setValue("admin123");
+        buttonLogin.click();
+
+    }
+
 }
